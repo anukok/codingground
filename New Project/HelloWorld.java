0c31de528e206eaff6a358da8e7932964e77035e
Anusha Kokkula <anusha.kokkula@gmail.com>
2016-02-07 21:09:26 -0600

    Created by Coding Ground Client

---
+++ New Project/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+        System.out.println("Hello World");
+        Rectangle r1 = new Rectangle(1,1,4,8);
+        Rectangle r1 = new Rectangle(1,1,4,8);
+     }
+}
